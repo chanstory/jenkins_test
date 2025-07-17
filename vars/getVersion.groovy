3ef809b62eaeb3c521916bfc5de5f4fb8bf62533
taechankim <xocksdlekd@gmail.com>
2025-07-17 17:28:17 +0900

    test

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -3,6 +3,7 @@ import java.io.File
 
 // 동일한 resourceName 의 JOB이 동시에 실행되지 않음을 가정하고 read 하는 함수에는 lock을 걸지 않음
 def call(String repositoryName) {
+    echo "update test"
     def filePath = "data_files/versions"
     def file = new File(filePath)
 
